274ebe1622cbbe873eedc81ab39a8ce4db44ac0a
Konijima <14941882+Konijima@users.noreply.github.com>
2018-09-10 17:04:14 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,24 +10,4 @@ let blockchain = new Blockchain()
 
 blockchain.on('initialized', () => {
   console.log('Blockchain has been initialized successfully!')
-
-  nodeP2P = new NodeP2P()
-
-  nodeP2P.on('init', () => {
-    console.log('P2P peerId has been generated!')
-  })
-
-  nodeP2P.on('ready', () => {
-    console.log('P2P has initialized!')
-    nodeP2P.start()
-  })
-
-  nodeP2P.on('start', () => {
-    console.log('P2P node has been started successfully!')
-  })
-
-  /* Blockchain debug
-  var newBlock = new Block(1, Date.now(), {})
-  blockchain.addBlock(newBlock)
-  console.log(blockchain)*/
 })
